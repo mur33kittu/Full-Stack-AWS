5c233d9ab51eeac7f351dfadbab05e4846ba2d81
Murthy Guruswamy <mur3kittu@gmail.com>
2019-12-25 13:10:37 -0500

    modified

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/common/header';
 import Footer from './components/common/footer';
-import './App.scss';
+import './app.scss';
 import router from './routes';
 
 function App() {
