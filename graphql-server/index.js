7ccfd361651a72fbcb09bac69a47fdc9ab84e8df
Murthy Guruswamy <mur3kittu@gmail.com>
2020-01-01 12:24:43 -0500

    PATCH: remove cors

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -11,14 +11,14 @@ const mongoose = require("mongoose");
 const path = require("path");
 const config = require("../config/config");
 const webpackConfig = require("../webpack.config");
-const cors = require("cors");
+// const cors = require("cors");
 const isDev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Configuration
 // ================================================================================================
